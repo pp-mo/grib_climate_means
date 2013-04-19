Add units +various fixes. Sort-of first working for hacked-Iris load.

climate_series_loading.py
@@ -137,10 +137,26 @@ def enumerate_all_results():
     return results
 
 
-def test():
-    results = enumerate_all_results()
-    print '\n'.join([str(entry) for entry in results])
+def pickout_spec(param, level=None):
+    aspecs = np.array(enumerate_all_results())
+    aspecs = aspecs[np.where(aspecs[:, 0] == str(param))]
+    if level is not None:
+        aspecs = aspecs[np.where(aspecs[:,1 ] == str(level))]
+    assert aspecs.shape[1] == 3
+    n_found = aspecs.shape[0]
+    if n_found != 1:
+        raise ValueError('Pickout(param={}, level={}) '
+                         'found {} results instead of 1 ?'.format(
+                             param, level, n_found))
+    return aspecs[0]
 
+
+def test():
+    spec = pickout_spec(186)
+    cube = cube_for_param_and_level(param=spec[0], level=spec[1])
+#    results = enumerate_all_results()
+#    print '\n'.join([str(entry) for entry in results])
+    t_dbg = 0
 
 if __name__ == '__main__':
     test()

climate_series_transformation.py
@@ -89,23 +89,17 @@ def main(output_dirpath=default_output_dirpath,
             print '  saving to \'{}\' ...'.format(file_path)
             iris.save(data_cube, file_path)
 
-def pickout_spec(param, level=None):
-    aspecs = np.array(csl.enumerate_all_results())
-    aspecs = aspecs[np.where(aspecs[:, 0] == str(param))]
-    if level is not None:
-        aspecs = aspecs[np.where(aspecs[:,1 ] == str(level))]
-    assert aspecs.shape[1] == 3
-    n_found = aspecs.shape[0]
-    if n_found != 1:
-        raise ValueError('Pickout(param={}, level={}) '
-                         'found {} results instead of 1 ?'.format(
-                             param, level, n_found))
-    return aspecs[0]
 
 if __name__ == '__main__':
-#    test_series = None
-#    test_series = [pickout_spec(157, 850)]
-#    test_series = [pickout_spec(186)]
-    test_series = [pickout_spec(167)]
+    do_test_only = True
+    if do_test_only:
+#        test_series = None
+#        test_series = [pickout_spec(157, 850)]
+#        test_series = [pickout_spec(186)]
+        test_series = [csl.pickout_spec(167)]
+    else:
+        test_series = None
 #    # NOTE: this fails in grib-pdt-deduce because latest transform is on 'month' not time
-    main(series_specs=test_series)  # , load_from_cubes=True)
+    main(series_specs=test_series,
+         load_from_cubes=False,
+         save_as_cubes=True)

simple_translations_grib1.py
@@ -22,7 +22,8 @@ GRIB1_TABLE2VERSIONS = {
     'ecmwf_local': 128,
 }
 
-GRIB1_CENTRECODES = {'ecmwf': 98}
+# NOTE: this code already 'interpreted' by grib.py from a number to a string
+GRIB1_CENTRECODES = {'ecmwf': 'ecmf'}
 
 #
 # These probably don't belong here ?
@@ -31,7 +32,8 @@ GRIB1_CENTRECODES = {'ecmwf': 98}
 #GRIB1_LEVELTYPES = {'surface': 1, 'isobaricInhPa': 100}
 
 class Grib1Code(object):
-    main_names = ['edition', 'table2version', 'centre', 'param', 'longname']
+    main_names = ['edition', 'table2version', 'centre', 'param',
+                  'longname', 'units']
     optional_names = {'set_height': np.NaN}
     all_names = main_names + optional_names.keys()
 
@@ -48,7 +50,7 @@ class Grib1Code(object):
 ECMWF_GRIB1_LOCALCODE_NAME_PREFIX = 'ecmwf_grib1_local__'
 
 class EcmwfLocalGrib1Code(Grib1Code):
-    def __init__(self, param, longname,
+    def __init__(self, param, longname, units,
                  edition=ECMWF_DEFAULT_GRIB1_EDITION,
                  table2version=GRIB1_TABLE2VERSIONS['ecmwf_local'],
                  centre=GRIB1_CENTRECODES['ecmwf'],
@@ -57,6 +59,7 @@ class EcmwfLocalGrib1Code(Grib1Code):
         super(EcmwfLocalGrib1Code, self).__init__(
             param=param,
             longname=ECMWF_GRIB1_LOCALCODE_NAME_PREFIX + longname,
+            units=units,
             edition=edition,
             table2version=table2version,
             centre=centre,
@@ -71,30 +74,30 @@ def add_ec_grib1(*args, **kwargs):
 
 # Add all the codes we're interested in
 # Use the set_height key to interpret codes that imply an AGL height
-add_ec_grib1(186, 'low_cloud')
-add_ec_grib1(187, 'medium_cloud')
-add_ec_grib1(187, 'high_cloud')
-add_ec_grib1(164, 'total_cloud_cover')
-add_ec_grib1(141, 'snow_depth')
-add_ec_grib1(151, 'mslp')
-add_ec_grib1(165, '10m_wind_x', set_height=10.0)
-add_ec_grib1(166, '10m_wind_y', set_height=10.0)
-add_ec_grib1(167, '2m_temp', set_height=2.0)
-add_ec_grib1(168, '2m_dewpoint', set_height=2.0)
+add_ec_grib1(186, 'low_cloud', '1')
+add_ec_grib1(187, 'medium_cloud', '1')
+add_ec_grib1(187, 'high_cloud', '1')
+add_ec_grib1(164, 'total_cloud_cover', '1')
+add_ec_grib1(141, 'snow_depth', 'm')
+add_ec_grib1(151, 'mslp', 'hPa')
+add_ec_grib1(165, '10m_wind_x', 'm s-1', set_height=10.0)
+add_ec_grib1(166, '10m_wind_y', 'm s-1', set_height=10.0)
+add_ec_grib1(167, '2m_temp', 'K', set_height=2.0)
+add_ec_grib1(168, '2m_dewpoint', 'K', set_height=2.0)
 
-add_ec_grib1(59, 'cape')
-add_ec_grib1(174, 'albedo')
-add_ec_grib1(34, 'sst')
-add_ec_grib1(31, 'sea_ice_cover')
-add_ec_grib1(173, 'surface_roughness')
-add_ec_grib1(235, 'skin_temperature')
+add_ec_grib1(59, 'cape', 'J Kg-1')
+add_ec_grib1(174, 'albedo', '1')
+add_ec_grib1(34, 'sst', 'K')
+add_ec_grib1(31, 'sea_ice_cover', '1')
+add_ec_grib1(173, 'surface_roughness', 'm')
+add_ec_grib1(235, 'skin_temperature', 'K')
 
-add_ec_grib1(157, 'relative_humidity')
-add_ec_grib1(130, 'temperature')
-add_ec_grib1(129, 'geopotential')
-add_ec_grib1(131, 'wind_u')
-add_ec_grib1(132, 'wind_v')
-add_ec_grib1(135, 'wind_z')
+add_ec_grib1(157, 'relative_humidity', '1')  # actually, this is %ge.  No unit
+add_ec_grib1(130, 'temperature', 'K')
+add_ec_grib1(129, 'geopotential', 'm^2 s-2')
+add_ec_grib1(131, 'wind_u', 'm s-1')
+add_ec_grib1(132, 'wind_v', 'm s-1')
+add_ec_grib1(135, 'wind_z', 'm s-1')
 
 # Convert to a recarray for easy searching.
 # NOTE: failed to create by concatenation -- bugs in numpy
@@ -103,7 +106,7 @@ _GRIB1_CODES_ARRAY = np.core.records.fromrecords(
     names=Grib1Code.all_names)
 
 
-def identify_grib1_key(edition, table2version, centre, param):
+def identify_grib1_key(edition, table2version, centre, param, debug=False):
     result_array = _GRIB1_CODES_ARRAY[
         np.where(np.logical_and(
             _GRIB1_CODES_ARRAY.edition == edition,
@@ -112,29 +115,38 @@ def identify_grib1_key(edition, table2version, centre, param):
                 np.logical_and(_GRIB1_CODES_ARRAY.centre == centre,
                                _GRIB1_CODES_ARRAY.param == param))))]
     n_results = len(result_array)
-    if n_results != 1:
+    if n_results > 1:
         raise ValueError('Ecmwf grib1 lookup for param \'{:s}\' '
                          'found {:d} matches'.format(param, n_results))
-    return result_array[0]
+    elif n_results == 1:
+        result = result_array[0]
+    else:
+        # none found
+        result = None
+    if debug:
+        r_str = str(result)
+        print 'STG1-identify: edition={} t2v={} centre={}, param={} ==> {:s}'.format(
+            edition, table2version, centre, param, r_str)
+    return result
 
-def test():
-    test_param = 186
-    test_name = ECMWF_GRIB1_LOCALCODE_NAME_PREFIX + 'low_cloud'
-    testcode = EcmwfLocalGrib1Code(9999, '__junk__')
+
+def test(debug=False):
+    test_param = 141
+    expected_name = ECMWF_GRIB1_LOCALCODE_NAME_PREFIX + 'snow_depth'
+    expected_units = 'm'
     testrec = identify_grib1_key(
         edition=ECMWF_DEFAULT_GRIB1_EDITION,
         table2version=GRIB1_TABLE2VERSIONS['ecmwf_local'],
         centre=GRIB1_CENTRECODES['ecmwf'],
-        param=test_param)
-    assert testrec.longname == test_name
+        param=test_param,
+        debug=debug)
+    assert testrec != None
+    assert testrec.longname == expected_name
+    assert testrec.units == expected_units
     assert np.isnan(testrec.set_height)
 
 
-#
-# Now do similar to translate ECMWF local-table phenomena into GRIB2 codes.
-#
-
 if __name__ == '__main__':
-    test()
+    test(debug=True)
     print '..Done ok.'
     t_dbg = 0